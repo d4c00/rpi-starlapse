5667bb6fb7b0ada7603abcd839b59a8ced038d1a
d4c00 <alicesekai1@gmail.com>
2026-04-07 07:08:56 +0800

    Increase DARK_FRAME_COUNT from 15 to 30

--- time-lapse/snippets/config.py
+++ time-lapse/snippets/config.py
@@ -31,7 +31,7 @@ AE_MARGIN = 1.5
 # Calibration Trigger
 DARK_TRIGGER_FILE = "/dev/shm/time-lapse/calibration"
 # Dark Frames
-DARK_FRAME_COUNT = 15
+DARK_FRAME_COUNT = 30
 # Bias Frames
 CAPTURE_BIAS_FRAMES = False
 BIAS_FRAME_COUNT = 100
