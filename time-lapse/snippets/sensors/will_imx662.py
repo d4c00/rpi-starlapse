ec12a0c63624f64d24d9082fc08e07d8dd83d283
d4c00 <alicesekai1@gmail.com>
2026-04-08 21:21:17 +0800

    Update will_imx662.py

--- time-lapse/snippets/sensors/will_imx662.py
+++ time-lapse/snippets/sensors/will_imx662.py
@@ -2,52 +2,53 @@
 # Licensed under the MIT License.
 import numpy as np
 
-# --- Basic Identity ---
 SENSOR_NAME = "imx662"
 MEDIA_ENTITY_NAME = "imx662 10-001a"
 WIDTH, HEIGHT = 1936, 1100
 MEDIA_CTL_FMT = "SRGGB12_1X12"
 V4L2_PIXELFORMAT = "RG12"
 
-# --- Physical Constraints ---
 RAW_BPP = 2
 EXP_OFFSET = 8
-MIN_EXPOSURE = 1.2e-5
-MAX_EXPOSURE = 1.0
-MIN_GAIN = 1
-MAX_GAIN = 480
-
-# --- V4L2 Control Mapping ---
-CORE_MAPPING = {
-    "exposure": "exposure",
-    "gain": "analogue_gain",
-    "vblank": "vertical_blanking",
-    "hblank": "horizontal_blanking",
-    "pixel_rate": "pixel_rate"
-}
-
-# --- Driver Extensions ---
-EXTENSIONS = {
-    "hcg_enable": 1,
-    "black_level": 64,
-    "horizontal_flip": 0,
-    "vertical_flip": 0
-}
+MIN_EXPOSURE = 1.0e-6 
+MAX_EXPOSURE = 1.0     
+MIN_GAIN = 34
+MAX_GAIN = 240
 
 VIRT_GAIN_MIN = 1.0
 VIRT_GAIN_MAX = 16.0
 
+_MAP = {
+    "exp": "exposure",
+    "gain": "analogue_gain",
+    "vblk": "vertical_blanking",
+    "hblk": "horizontal_blanking",
+    "pr": "pixel_rate"
+}
+
+EXTENSIONS = {
+    "hcg_enable": 1,
+    "brightness": 50,
+    "horizontal_flip": 0,
+    "vertical_flip": 0
+}
+
 def get_runtime_ctrls(target_us, gain, container):
-    pixel_rate = container.hw_inventory[CORE_MAPPING['pixel_rate']]['val']
-    hblank = container.hw_inventory[CORE_MAPPING['hblank']]['val']
-    hmax = WIDTH + hblank
+    inv = container.hw_inventory
+    pix_rate = inv[_MAP['pr']]['val']
+    hmax = WIDTH + inv[_MAP['hblk']]['val']
 
-    req_vmax = int((target_us / 1000.0 * pixel_rate) / (1000 * hmax))
+    req_vmax = int((target_us / 1000.0 * pix_rate) / (1000 * hmax))
 
-    v_key, e_key, g_key = CORE_MAPPING['vblank'], CORE_MAPPING['exposure'], CORE_MAPPING['gain']
+    ctrls = []
+    
+    v_val = int(np.clip(req_vmax - HEIGHT, inv[_MAP['vblk']]['min'], inv[_MAP['vblk']]['max']))
+    ctrls.append((_MAP['vblk'], v_val))
+    
+    e_val = int(np.clip(req_vmax - EXP_OFFSET, inv[_MAP['exp']]['min'], inv[_MAP['exp']]['max']))
+    g_val = int(np.clip(gain, inv[_MAP['gain']]['min'], inv[_MAP['gain']]['max']))
 
-    return {
-        v_key: int(np.clip(req_vmax - HEIGHT, container.hw_inventory[v_key]['min'], container.hw_inventory[v_key]['max'])),
-        e_key: int(np.clip(req_vmax - EXP_OFFSET, container.hw_inventory[e_key]['min'], container.hw_inventory[e_key]['max'])),
-        g_key: int(np.clip(gain, container.hw_inventory[g_key]['min'], container.hw_inventory[g_key]['max']))
-    }
+    ctrls.append((_MAP['exp'], e_val))
+    ctrls.append((_MAP['gain'], g_val))
+    
+    return ctrls
