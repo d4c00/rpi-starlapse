b3abe76b2225c8901de069692f01cbb7aaa8d481
d4c00 <alicesekai1@gmail.com>
2026-04-15 12:25:56 +0800

    Update imx662.py

--- time-lapse/snippets/sensors/imx662.py
+++ time-lapse/snippets/sensors/imx662.py
@@ -46,6 +46,7 @@ def find_nodes():
     return v_node, s_node
 
 _v_n, _s_n = find_nodes()
+
 _out = subprocess.check_output(f"v4l2-ctl -d {_s_n} --list-ctrls", shell=True, text=True)
 _f = lambda n, f: int(re.search(rf"{n}.*?{f}=(\d+)", _out).group(1))
 
@@ -59,11 +60,8 @@ def _calculate_phys_exposure(exp_lines, mode="min"):
     return (exp_lines * (WIDTH + h_blank)) / pixel_rate
 
 MIN_EXPOSURE = _calculate_phys_exposure(_f("exposure", "min"), mode="min")
-
 v_total_max = HEIGHT + _f("vertical_blanking", "max")
-
 MAX_EXPOSURE = _calculate_phys_exposure(v_total_max, mode="max")
-
 AE_MIN_US = int(MIN_EXPOSURE * 1e6)
 
 def print_hardware_info():
@@ -74,10 +72,11 @@ def print_hardware_info():
 print_hardware_info()
 
 def get_init_cmds():
-    v_node, s_node = find_nodes()
-    if not s_node: return []
+    if not _s_n:
+        return []
 
-    subdev_name = os.path.basename(s_node)
+    subdev_name = os.path.basename(_s_n)
+
     with open(f"/sys/class/video4linux/{subdev_name}/name", 'r') as f:
         full_entity_name = f.read().strip()
 
@@ -89,13 +88,15 @@ def get_init_cmds():
                 if SENSOR_NAME in out:
                     m_node = f"/dev/media{i}"
                     break
-            except: pass
+            except:
+                pass
 
     return [
         f"media-ctl -d {m_node} -V '\"{full_entity_name}\":0 [fmt:{MBUS_FORMAT}/{WIDTH}x{HEIGHT} field:none]'",
         f"media-ctl -d {m_node} -V '\"{full_entity_name}\":0 [crop:(0,0)/{WIDTH}x{HEIGHT}]'",
-        f"v4l2-ctl -d {v_node} --set-fmt-video=width={WIDTH},height={HEIGHT},pixelformat={V4L2_PIXELFORMAT}",
-        f"v4l2-ctl -d {s_node} --set-ctrl \"HCG Enable\"=1"]
+        f"v4l2-ctl -d {_v_n} --set-fmt-video=width={WIDTH},height={HEIGHT},pixelformat={V4L2_PIXELFORMAT}",
+        f"v4l2-ctl -d {_s_n} --set-ctrl \"HCG Enable\"=1"   # 初始化时强制开启 HCG
+    ]
 
 def get_runtime_cmds(target_us, gain, container):
     out = subprocess.check_output(f"v4l2-ctl -d {container.s_node} --list-ctrls", shell=True, text=True)
