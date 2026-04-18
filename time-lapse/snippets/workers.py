fb9f921a7dbf51ce4e3b1eabc7a859a4a2ea4a58
d4c00 <alicesekai1@gmail.com>
2026-04-18 17:26:16 +0800

    Update workers.py

--- time-lapse/snippets/workers.py
+++ time-lapse/snippets/workers.py
@@ -53,7 +53,7 @@ def capture_frame(cam, mode, target, r_path, sh_frame_id, sh_last_ae_id, is_onli
 def camera_worker(sh_frame_id, sh_last_ae_id, data_q, stop_ev, trigger_ev, sh_snap, is_online, ready_ev, sh_dev_id, pause_ev, sh_cam_en):
     dev_id_str = sh_dev_id.value.decode().rstrip('\x00')
     w_path, r_path = get_shm_paths(dev_id_str)
-    hist_q = [unpack_snap(sh_snap.value)] * 3
+    hist_q = [unpack_snap(sh_snap.value)] * 2
 
     try:
         cam = V4L2Camera()
@@ -75,7 +75,7 @@ def camera_worker(sh_frame_id, sh_last_ae_id, data_q, stop_ev, trigger_ev, sh_sn
             blink_loop(30, 0.2, 0.2) 
  
             flush_old_frames(cam)
-            hist_q = [unpack_snap(sh_snap.value)] * 3
+            hist_q = [unpack_snap(sh_snap.value)] * 2
             logger.info(f">>> [1/2] Capturing Darks (Count:{DARK_FRAME_COUNT})")
             for _ in range(DARK_FRAME_COUNT):
                 if stop_ev.is_set(): break
@@ -90,7 +90,7 @@ def camera_worker(sh_frame_id, sh_last_ae_id, data_q, stop_ev, trigger_ev, sh_sn
                               sh_frame_id, sh_last_ae_id, is_online, sh_dev_id, sh_snap, data_q, hist_q)
 
             flush_old_frames(cam)
-            hist_q = [unpack_snap(sh_snap.value)] * 3
+            hist_q = [unpack_snap(sh_snap.value)] * 2
             if CAPTURE_BIAS_FRAMES:
                 logger.info(f">>> [2/2] Capturing Biases (Count: {BIAS_FRAME_COUNT})")
                 bias_cfg = {"t_us": int(sensor.MIN_EXPOSURE * 1e6), "g": sensor.MIN_GAIN}
