d262103046b7c8d23c5cf76778b74fee628cede3
d4c00 <alicesekai1@gmail.com>
2026-04-20 01:10:42 +0800

    Update ae.py

--- time-lapse/snippets/ae.py
+++ time-lapse/snippets/ae.py
@@ -123,12 +123,11 @@ class AdaptiveExposureEngine:
         actual_ev = math.log2(actual_us / 1e6) + math.log2(self._phys_to_virt_gain(actual_reg))
 
         ev_step = self._compute_ev_step(luma)
-        ideal_ev = np.clip(actual_ev + ev_step, self.MIN_EV, self.MAX_EV)
 
         latest_ev = math.log2((current_us * self._phys_to_virt_gain(current_reg_gain) / 1e6) + 1e-9)
         remaining = ideal_ev - latest_ev
 
-        delta = np.clip(remaining, self.LIMIT_DN, self.LIMIT_UP)
+        delta = self._update_controller(remaining)
 
         target_ev = np.clip(latest_ev + delta, self.MIN_EV, self.MAX_EV)
         
