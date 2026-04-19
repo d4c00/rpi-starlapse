eb1650ac009513888d53593ce1995825967af071
d4c00 <alicesekai1@gmail.com>
2026-04-19 23:20:09 +0800

    Update ae.py

--- time-lapse/snippets/ae.py
+++ time-lapse/snippets/ae.py
@@ -47,9 +47,7 @@ class AdaptiveExposureEngine:
     def _phys_to_virt_gain(self, reg_val):
         reg_val = np.clip(reg_val, self.REG_MIN, self.REG_MAX)
         if self.REG_MAX == self.REG_MIN: return 1.0
-
         db_offset = (reg_val - self.REG_MIN) * (self.GAIN_DB_MAX - self.GAIN_DB_MIN) / (self.REG_MAX - self.REG_MIN)
-
         virt_gain = 10 ** (db_offset / 20.0)
         return np.clip(virt_gain, self.MIN_VIRT_GAIN, self.MAX_VIRT_GAIN)
 
